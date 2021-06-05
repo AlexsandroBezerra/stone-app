3f4d3135e0bf4a0399c1303550558f9fa6b6a881
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-06-05 15:52:15 -0300

    refact: Add new background

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,5 @@ import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <>
-      <img src="/graph.svg" alt="Círculos" className="graphImageBackground" />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
